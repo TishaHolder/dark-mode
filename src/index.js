b35b502ae44c1c1fc06cf934e817994249ed92bb
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-04 10:30:25 -0400

    added useLocalStorage

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import useLocalStorage from "./hooks/useLocalStorage.js";
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [local, setLocal] = useLocalStorage("testkey", [1, 2, 3]);
+ 
 
   useEffect(() => {
+    
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
@@ -20,6 +24,7 @@ const App = () => {
   }, []);
   return (
     <div className="App">
+      {JSON.stringify(local)}
       <Navbar />
       <Charts coinData={coinData} />
     </div>
